755e7111c852a1b0d2e7cb19c3f907cfe1bd7cf3
tech-noe <ktc24a31g0017@edu.kyoto-tech.ac.jp>
2024-12-04 00:18:23 +0900

    5章まで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,56 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import { count } from "console";
+import { getMembersList } from "../_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "../_constants";
+import styles from "./page.module.css"
 
-const data = {
-    contents: [
-        {
-            id: "1",
-            image: {
-                url: "/img-member1.jpg",
-                width: 240,
-                height: 240,
-            },
-            name: "デイビッド・チャン",
-            position: "CEO",
-            profile: `
-                グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフト
-                ウェア企業の上級幹部として勤務し、都市場進出や収益成長に成功。自身の経験と洞察力によ
-                り、業界のトレンドを見極めて戦略的な方針を策定し、会社の成長を引率している。
-            `,
-        },
-        {
-            id: "2",
-            image: {
-                url: "/img-member2.jpg",
-                width: 240,
-                height: 240,
-            },
-            name: "エミリー・サンダース",
-            position: "CCO",
-            profile:
-            `グローバル企業での運営管理と組織改革の経験豊富なエグゼクティブ。以前は製
-            造業界CCOとして勤務し、生産効率の向上や品質管理の最適化に成功。戦略的なマインドセ
-            ット組織の能力強化に対する専門知識は、会社の成長と効率化に大きく貢献している。`,
-        },
-        {
-            id: "3",
-            image: {
-                url: "/img-member3.jpg",
-                width: 240,
-                height: 240,
-            },
-            name: "ジョン・ウィルソン",
-            position: "CTO",
-            profile:
-            `先端技術の研究開発と製品イノベーションの分野で優れた経歴を持つテクノロジ
-            ーエキスパート。以前は、大手テクノロジー企業の研究開発部門で主任エンジニアとして勤務
-            し、革新的な製品の開発に携わった。最新の技術トレンドに精通し、当社の製品ポートフォリ
-            オを革新的かつ闘争力のあるものにするためにリサーチと開発をリードしている。`, 
-        },
-    ],
-};
-
-export default function Page() {
+export default async function Page() {
+    const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
     return (
         <div className={styles.container}>
             {data.contents.length === 0 ? (
