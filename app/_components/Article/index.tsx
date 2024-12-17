ef267bdcbe8341d42aae52515ebf028abc876620
tech-noe <ktc24a31g0017@edu.kyoto-tech.ac.jp>
2024-12-17 14:39:03 +0900

    7-2ページネーション

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 import Image from "next/image";
 import type { News } from "@/app/_libs/microcms";
-import Category from "../Category"; // Corrected the import for Category
-import Date from "../Date"; // Corrected the import path for Date
+import Category from "../Category"; 
+import Date from "../Date";
 import styles from "./index.module.css";
 
 type Props = {
@@ -14,7 +15,12 @@ export default function Article({ data }: Props) {
       <h1 className={styles.title}>{data.title}</h1>
       <p className={styles.description}>{data.description}</p> 
       <div className={styles.meta}>
+        <Link
+            href={`/news/category/${data.category.id}`}
+            className={styles.categoryLink}
+        >
         <Category category={data.category} />
+        </Link>
         <Date date={data.publishedAt ?? data.createdAt} />
       </div>
       {data.thumbnail && (
