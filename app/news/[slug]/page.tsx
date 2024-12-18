a3f8650361c78443f2cda1cfc2d9d9020593d3a9
tech-noe <ktc24a31g0017@edu.kyoto-tech.ac.jp>
2024-12-18 15:10:58 +0900

    ７章まで完了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -3,15 +3,22 @@ import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 type Props = {
     params: {
       slug: string;
     };
+    searchParams: {
+        dk?: string;
+    };
   };
+
   
-  export default async function Page({params}: Props) {
-    const data = await getNewsDetail(params.slug).catch(notFound);
+  export default async function Page({params, searchParams }: Props) {
+    const data = await getNewsDetail(params.slug,{
+        draftKey: searchParams.dk,
+    }).catch(notFound);
 
     return (
         <>
