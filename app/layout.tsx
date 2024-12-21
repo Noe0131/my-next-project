61ddbae282c343cc11de450e97c23ac70099203c
tech-noe <ktc24a31g0017@edu.kyoto-tech.ac.jp>
2024-12-21 11:01:16 +0900

    サイト全体にrevalidateを設定する

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
-
 import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
+export const revalidate = 60;
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ja">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>My Website</title>
+      </head>
       <body>
         <Header />
         {children}
